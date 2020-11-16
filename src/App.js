d0c1a0933d16a710aed3e117cde1b1f068713952
ZiP <thezip33@gmail.com>
2020-11-16 16:58:11 +0200

    first msg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Счетчик: </h1>
+      <button className='btn btn-success'>Увеличить</button>
+      <button className='btn btn-danger'>Уменьшить</button>
     </div>
   );
 }
